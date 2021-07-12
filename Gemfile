420105fa42e4bdb83ba40a06c0c74db840ec2277
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-12 16:44:23 +0100

    Implemented sinatra reloader, added more paths and cute kitten

--- Gemfile
+++ Gemfile
@@ -7,4 +7,5 @@ ruby '3.0.0'
 group :development, :test do
   gem 'sinatra'
   gem 'webrick'
+  gem 'sinatra-contrib'
 end
--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
-    "Hello World"
-  end
+    "I'm a little webapp short and stout! Here's my port, here's my route!"
+end
+
+get '/sendhelp' do
+    "It's dangerous to go alone, take this!"
+end
+
+get '/cat' do
+    "<img src='https://i.imgur.com/jFaSxym.png' alt='Cute kitten!' style='border: 1px dashed red'>"
+end
+
+get '/secret' do
+    "Shhhhh!"
+end
+
+get '/supersecret' do
+    "How did you find me?!"
+end
